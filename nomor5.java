846cddf68955888f3004c8b0299a2aa182186374
Muhammad Rifki Setiawan <37269168+rifkistwn@users.noreply.github.com>
2018-11-17 20:55:50 +0700

    nomor5.java

---
+++ nomor5.java
@@ -0,0 +1,7 @@
+public class nomor5 {
+    public static void main(String[] args) {
+        String str1 = "kelompok";
+        String str2x = str1.replace('o', 'a');
+        System.out.println(str2x);
+    }
+}
