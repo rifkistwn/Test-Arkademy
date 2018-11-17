7743c13ea1c71284294512a498a5cf92c2602f17
Muhammad Rifki Setiawan <37269168+rifkistwn@users.noreply.github.com>
2018-11-17 13:01:56 +0700

    nomor1.js

---
+++ nomor1.js
@@ -0,0 +1,17 @@
+const json = {
+ itemId : "12341822",
+    itemName: "FGX Flannel Shirt",
+ price : 195000,
+ availableColorAndSize :[{ 
+   color : "blue-black", 
+    size : "M, L, XL"
+  },
+       {
+   color : "black-white", 
+    size : "L"
+  }
+],
+ freeShiping : false
+  } 
+ 
+ return json;
